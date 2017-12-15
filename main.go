69d66d5acb22d7847c841ddf2ea3264c7dad4ad3
wanghongfei <brucewhf@gmail.com>
2017-12-15 15:55:42 +0800

    fix

--- main.go
+++ main.go
@@ -7,25 +7,35 @@ import (
 	"gaecharge/biz/report"
 	"log"
 	"sync"
-	"time"
 	"gaecharge/config"
+	_ "net/http/pprof"
+	"net/http"
 )
 
 func main() {
-	time.Sleep(time.Second * 10)
-
 	var wg sync.WaitGroup
 
 	// kafak监听
-	go func() {
-		wg.Add(1)
-		kafkalog.StartConsumer(biz.ConsumeMessage)
-	}()
+	wg.Add(1)
+	startKafkaConsumer()
 
 	// 报表定时任务
-	go func() {
-		wg.Add(1)
+	wg.Add(1)
+	startReportTask()
 
+
+	startProfiling()
+	wg.Wait()
+}
+
+func startKafkaConsumer() {
+	go func() {
+		kafkalog.StartConsumer(biz.ConsumeMessage)
+	}()
+}
+
+func startReportTask() {
+	go func() {
 		for {
 			ticker := cron.NewHourTicker(config.AppConfig.Report.ExeMinute, config.AppConfig.Report.ExeSecond)
 			ticker.Tick() // block
@@ -37,5 +47,12 @@ func main() {
 		}
 	}()
 
-	wg.Wait()
+}
+
+func startProfiling() {
+	// pprof.StartCPUProfile(f)
+
+	go func() {
+		http.ListenAndServe("localhost:9000", nil)
+	}()
 }
